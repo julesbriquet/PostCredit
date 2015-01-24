766f9f465a4a0db075b3eb1860442fcb2a2a81fd
Damien <baal.chouchen@gmail.com>
2015-01-24 14:18:00 +0100

    Add plateformer, jumper and coin scenes

---
+++ Game/Assets/Scripts/plateformer/PlateformerAttacker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlateformerAttacker : MonoBehaviour {
+
+	public float Speed;
+	
+
+	
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Vector2 velocity = this.rigidbody2D.velocity;
+
+		velocity.x = Speed;
+
+		this.rigidbody2D.velocity = velocity;
+	}
+	
+	void OnCollisionEnter2D ( Collision2D coll )
+	{
+		if(coll.collider.tag == "Player")
+		{
+			coll.collider.gameObject.SendMessage("DeadAttacker");
+			Physics2D.IgnoreCollision(this.collider2D, coll.collider.collider2D,true);
+		}
+	}
+}
---
+++ Game/Assets/Scripts/plateformer/PlateformerFireSkull.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlateformerFireSkull : MonoBehaviour {
+
+	public float Height;
+	public float Speed;
+
+	float top;
+	float bottom;
+
+	int direction = 1;
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.top = this.transform.position.y + this.Height;
+		this.bottom = this.transform.position.y - this.Height;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		this.rigidbody2D.velocity = new Vector2(0f, Speed * this.direction);
+
+		if(direction > 0)
+		{
+			if(this.transform.position.y >= this.top)
+			{
+				direction *= -1;
+			}
+		}
+		else
+		{
+			if(this.transform.position.y <= this.bottom)
+			{
+				direction *= -1;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D ( Collider2D other )
+	{
+		if(other.tag == "Player")
+		{
+			other.gameObject.SendMessage("DeadSkull");
+			this.collider2D.enabled = false;
+		}
+	}
+
+
+}
