0f38ce2fea05f7a44c2769308a05bf37f19e1ac0
julesbriquet <jules.briquet@gmail.com>
2015-01-24 03:58:55 +0100

    Adding first step for finish him game

---
+++ Game/Assets/Scripts/finish_him/CommandUICreator.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class CommandUICreator : MonoBehaviour {
+
+    public Image upImage;
+    public Image downImage;
+    public Image leftImage;
+    public Image rightImage;
+    public Image actionImage;
+
+
+    public void GenerateUICommand(string commandList)
+    {
+
+        int imageWidth = (int)upImage.rectTransform.rect.width;
+        Debug.Log(imageWidth);
+
+        int startPosition = -(imageWidth / 2) * (commandList.Length - 1);
+        int currentPosition = startPosition;
+
+        RectTransform currentRectTransf = this.GetComponent<RectTransform>();
+
+        for (int i = 0; i < commandList.Length; i++)
+        {
+            Image instanciateImg = null;
+            Debug.Log(commandList);
+
+            if (commandList[i] == 'U')
+                instanciateImg = Instantiate(upImage, new Vector3(currentPosition, 0, 0), Quaternion.identity) as Image;
+            if (commandList[i] == 'D')
+                instanciateImg = Instantiate(downImage, new Vector3(currentPosition, 0, 0), Quaternion.identity) as Image;
+            if (commandList[i] == 'L')
+                instanciateImg = Instantiate(leftImage, new Vector3(currentPosition, 0, 0), Quaternion.identity) as Image;
+            if (commandList[i] == 'R')
+                instanciateImg = Instantiate(rightImage, new Vector3(currentPosition, 0, 0), Quaternion.identity) as Image;
+            if (commandList[i] == 'A')
+                instanciateImg = Instantiate(actionImage, new Vector3(currentPosition, 0, 0), Quaternion.identity) as Image;
+
+
+            if (instanciateImg != null)
+            {
+                Debug.Log(currentPosition);
+                instanciateImg.rectTransform.SetParent(this.transform, false);
+                //instanciateImg.rectTransform.position = new Vector3(currentPosition, instanciateImg.rectTransform.position.y, instanciateImg.rectTransform.position.z);
+            }
+
+            currentPosition += imageWidth;
+            
+        }
+
+    }
+}
---
+++ Game/Assets/Scripts/finish_him/FinishHimManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishHimManager : MonoBehaviour {
+
+    /*
+     * COMMAND:
+     * 1 = UP
+     * 2 = DOWN
+     * 3 = LEFT
+     * 4 = RIGHT
+     * 
+     * So LEFT DOWN RIGHT UP will be: 3241
+     */
+
+    public string commandToDo = "";
+
+    public CommandUICreator uiCreator;
+
+	// Use this for initialization
+	void Start () {
+
+        commandToDo = "";
+
+        // Level 1 & 2
+        if (GameManager.Instance.LevelDifficulty < 3)
+        {
+            commandToDo += generateStringCommand(GameManager.Instance.LevelDifficulty * 2);
+        }
+        else if (GameManager.Instance.LevelDifficulty == 3)
+        {
+            commandToDo += generateStringCommand(4);
+            commandToDo += generateStringCommand(2);
+        }
+
+        uiCreator.GenerateUICommand(commandToDo);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    string generateStringCommand(int numberOfDirection)
+    {
+        string returnedCommand = "";
+
+        for (int i = 0; i < numberOfDirection; i++)
+        {
+            int rand = Random.Range(1, 4);
+            returnedCommand += numberToCharCommand(rand);
+        }
+
+        // Adding action command
+        returnedCommand += 'A';
+
+        return returnedCommand;
+    }
+
+    char numberToCharCommand(int commandNumber)
+    {
+        if (commandNumber == 1)
+            return 'U';
+        else if (commandNumber == 2)
+            return 'D';
+        else if (commandNumber == 3)
+            return 'L';
+        else if (commandNumber == 4)
+            return 'R';
+
+        return 'B';
+    }
+}
