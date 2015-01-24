552d61347104c46813fd3d47dbba76b47e563dd5
julesbriquet <jules.briquet@gmail.com>
2015-01-24 15:46:22 +0100

    Assets integration for pong

--- Game/Assets/Scripts/pong/PongManager.cs
+++ Game/Assets/Scripts/pong/PongManager.cs
@@ -8,8 +8,12 @@ public class PongManager : MonoBehaviour {
 
     public PongBall ballToSpawn;
 
+    public Transform background;
+
 	// Use this for initialization
 	void Start () {
+
+        ResizeBackGround();
 
         for (int i = 0; i < GameManager.Instance.LevelDifficulty; i++) {
             PongBall ball = Instantiate(ballToSpawn, Vector3.zero, Quaternion.identity) as PongBall;
@@ -28,4 +32,30 @@ public class PongManager : MonoBehaviour {
         if (timeUntilEndGame <= 0)
             GameManager.Instance.LevelEnd(true);
 	}
+
+
+    void ResizeBackGround()
+    {
+        SpriteRenderer sr = background.GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        background.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        Vector3 xWidth = background.localScale;
+        xWidth.x = worldScreenWidth / width;
+        background.localScale = xWidth;
+        //transform.localScale.x = worldScreenWidth / width;
+        Vector3 yHeight = background.localScale;
+        yHeight.y = worldScreenHeight / height;
+        background.localScale = yHeight;
+        //transform.localScale.y = worldScreenHeight / height;
+
+    }
 }
