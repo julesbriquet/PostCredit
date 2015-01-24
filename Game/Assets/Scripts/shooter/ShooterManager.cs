6bbbd8099364e53b719c79d958c09c184d0a5119
julesbriquet <jules.briquet@gmail.com>
2015-01-24 03:17:02 +0100

    Timer for Shooter Game

--- Game/Assets/Scripts/shooter/ShooterManager.cs
+++ Game/Assets/Scripts/shooter/ShooterManager.cs
@@ -6,14 +6,21 @@ public class ShooterManager : MonoBehaviour {
     public EnnemyEntity[] ennemyList;
     public Transform SpawnPoint;
 
+    public float[] timerForDifficulty;
+    private float timeUntilEndGame;
+
 	// Use this for initialization
 	void Start () {
         
         Instantiate(ennemyList[GameManager.Instance.LevelDifficulty - 1], SpawnPoint.position, Quaternion.identity);
+        timeUntilEndGame = timerForDifficulty[GameManager.Instance.LevelDifficulty - 1];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        timeUntilEndGame -= Time.deltaTime;
+
+        if (timeUntilEndGame == 0)
+            GameManager.Instance.LevelEnd(false);
 	}
 }
