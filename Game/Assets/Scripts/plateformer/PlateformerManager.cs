a0405653453a198f9ebc2fe1e7b1065988864236
Damien <baal.chouchen@gmail.com>
2015-01-24 14:44:56 +0100

    Add timer in Plateformer/Jump and Coin

--- Game/Assets/Scripts/plateformer/PlateformerManager.cs
+++ Game/Assets/Scripts/plateformer/PlateformerManager.cs
@@ -6,6 +6,8 @@ public class PlateformerManager : MonoBehaviour {
 	public float MoveSpeed;
 	public float JumpForce;
 	public float Gravity;
+	public float TimerForDifficulty;
+	float timeUntilEndGame;
 
 	public static PlateformerManager Instance;
 
@@ -17,12 +19,24 @@ public class PlateformerManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
+		this.timeUntilEndGame = Time.time + TimerForDifficulty;
 		Physics2D.gravity = new Vector2(0f, this.Gravity);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if(Time.time > this.timeUntilEndGame)
+		{
+			if(GameObject.FindGameObjectWithTag("Player").GetComponent<PlateformerCharacter>().LockMove)
+			{
+				GameManager.Instance.LevelEnd(true);
+			}
+			else
+			{
+				GameManager.Instance.LevelEnd(false);
+			}
+		}
 	}
 
 	public float GetJumpForce ()
