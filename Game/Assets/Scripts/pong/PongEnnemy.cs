7a4a5e8cf57d949bd53e602e6e68dcb9e4de85e3
julesbriquet <jules.briquet@gmail.com>
2015-01-25 01:19:56 +0100

    Ennemy move in pong

--- Game/Assets/Scripts/pong/PongEnnemy.cs
+++ Game/Assets/Scripts/pong/PongEnnemy.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class PongEnnemy : MonoBehaviour {
 
+    public float speed;
+    private int direction;
+
 	// Use this for initialization
 	void Start () {
-	
+        direction = 1;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        if (this.transform.position.y > 2 || this.transform.position.y < -2)
+            direction *= -1;
+
+        rigidbody2D.velocity = new Vector2(0, speed * direction * Time.deltaTime);
+
 	}
 }
